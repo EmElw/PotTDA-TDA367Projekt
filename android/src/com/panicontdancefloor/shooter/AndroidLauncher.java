a57d3e5bcd4f4af3e535689ec4139e171a5dc7fb
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-03-28 15:30:39 +0200

    Fixed .gitignore

--- android/src/com/panicontdancefloor/shooter/AndroidLauncher.java
+++ android/src/com/panicontdancefloor/shooter/AndroidLauncher.java
@@ -7,12 +7,12 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.panicontdancefloor.shooter.Shooter;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useAccelerometer = false;
         config.useCompass = false;
-		initialize(new Shooter(), config);
-	}
+        initialize(new Shooter(), config);
+    }
 }
