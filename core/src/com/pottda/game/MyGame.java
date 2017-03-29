5f2112e2b310529c30354264cce1ee61a6106cd0
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-03-29 16:43:04 +0200

    Fixed files

--- core/src/com/pottda/game/MyGame.java
+++ core/src/com/pottda/game/MyGame.java
@@ -6,35 +6,23 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-<<<<<<< HEAD:core/src/com/pottda/game/MyGame.java
 public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-=======
-public class Shooter extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
->>>>>>> 3ad64f2cf269b5f587aaa9148cadf0e27f6a873c:core/src/com/panicontdancefloor/shooter/Shooter.java
+    private SpriteBatch batch;
+    private Texture img;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        //img = new Texture("badlogic.jpg");
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        //batch.begin();
+        //batch.draw(img, 0, 0);
+        //batch.end();
     }
 
     @Override
