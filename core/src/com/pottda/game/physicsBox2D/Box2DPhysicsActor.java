8766468bda29180cd3176d0f2ce2fcd2fa86552e
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-05 21:10:00 +0200

    Added Box2D-implementation of PhysicsActor for static objects

---
+++ core/src/com/pottda/game/physicsBox2D/Box2DPhysicsActor.java
@@ -0,0 +1,25 @@
+package com.pottda.game.physicsBox2D;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.pottda.game.model.PhysicsActor;
+
+import javax.vecmath.Vector2f;
+
+public class Box2DPhysicsActor extends PhysicsActor {
+    public Body body;
+
+    @Override
+    public Vector2f getPosition() {
+        Vector2 bodyPos = body.getPosition();
+        return new Vector2f(bodyPos.x, bodyPos.y);
+    }
+
+    /**
+     * Should be overridden by bodies that can change their movement vector
+     * @param movementVector
+     */
+    @Override
+    public void giveMovementVector(Vector2f movementVector) {
+    }
+}
