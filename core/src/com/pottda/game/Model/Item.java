d846b7e9747e58c52d0741e1c6dda79c61a071e9
EmElw <wamby@student.chalmers.se>
2017-05-06 01:12:31 +0200

    Implement spatial properties of Items, handling rotation within an inventory

--- core/src/com/pottda/game/Model/Item.java
+++ core/src/com/pottda/game/Model/Item.java
@@ -1,9 +1,12 @@
 package com.pottda.game.model;
 
+import com.pottda.game.Util;
+
 import javax.vecmath.Point2i;
 import javax.vecmath.Vector2f;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -11,29 +14,43 @@ import java.util.List;
  */
 
 public abstract class Item {
-    int orientation;
+    /**
+     * A list of offsets for physical positions
+     */
+    protected List<int[]> unrotatedRelativePositions;
 
-    List<Point2i> positions;
+    public int orientation;
 
-    public void init() {
-        positions = new ArrayList<Point2i>();
-    }
-
-    public int getOrientation() {
-        return orientation;
-    }
-
-    public int getOrientetationn() {
-        return orientetationn;
-    }
-
-    List<Item> nextItem;
     public int x;
     public int y;
-    private int orientetationn;
+
+    public void init() {
+        unrotatedRelativePositions = new ArrayList<int[]>();
+    }
+
+    /**
+     * Returns a list of {@code Integer}, where each corresponds to a space where this item is.
+     * <p>
+     * The function sequences a coordinate grid with width {@code w} into a single number so
+     * that i.e. (1,2) with w = 5 is equivalent to 1 + (2*5) = 11
+     *
+     * @param w the width of the grid
+     * @return a {@code List<Integer>}
+     */
+
+    public List<Integer> getPositionsAsIntegers(int w) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int[] p : unrotatedRelativePositions) {
+            int[] rotatedPoint = rotate(new int[]{p[0], p[1]}, orientation);
+            int v = (rotatedPoint[0] + x) +    // Add x to convert to absolute coordinate in Inventory
+                    (rotatedPoint[1] + y) * w;     // Multiply to add the whole number of rows
+            list.add(v);
+        }
 
 
-    public void setOrientetationn(int orientetationn) {
-        this.orientetationn = orientetationn;
+        //list.sort((Integer o1, Integer o2) -> (o1.compareTo(o2)));    // Does not work without API level 25
+        Util.sortIntegerList(list, true);
+        return list;
     }
 }
