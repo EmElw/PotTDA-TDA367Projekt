0c567f43ddb1b8e19f59b57e642e07cc2c32c2c5
EmElw <wamby@student.chalmers.se>
2017-05-06 15:10:39 +0200

    Add a ProjectileListenerAdapter for convenience

---
+++ core/src/com/pottda/game/model/ProjectileListenerAdapter.java
@@ -0,0 +1,36 @@
+package com.pottda.game.model;
+
+/**
+ * Created by Magnus on 2017-05-06.
+ */
+public abstract class ProjectileListenerAdapter implements ProjectileListener {
+    @Override
+    public void onAttack() {
+
+    }
+
+    @Override
+    public void onHit() {
+
+    }
+
+    @Override
+    public void onDestruction() {
+
+    }
+
+    @Override
+    public void onTimer() {
+
+    }
+
+    @Override
+    public void setTimer(int timer, boolean bool) {
+
+    }
+
+    @Override
+    public void onDistanceTraveled(int distance, boolean bool) {
+
+    }
+}
