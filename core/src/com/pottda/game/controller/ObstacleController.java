7c9c6bc100f0f4328fd0b00c587d052059be08ad
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-08 17:07:17 +0200

    Added a controller for obstacle only to tie the view and model together.

---
+++ core/src/com/pottda/game/controller/ObstacleController.java
@@ -0,0 +1,18 @@
+package com.pottda.game.controller;
+
+import com.pottda.game.model.ModelActor;
+import com.pottda.game.view.ViewActor;
+
+/**
+ * Created by gusta on 2017-05-08.
+ */
+public class ObstacleController extends AbstractController {
+    public ObstacleController(ModelActor modelActor, ViewActor viewActor) {
+        super(modelActor, viewActor);
+    }
+
+    @Override
+    public void onNewFrame() {
+
+    }
+}
