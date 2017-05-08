32a814351d2a45dc6b84c57d1bde2c302686f992
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-08 16:40:00 +0200

    Started implementing ActorFactory with Box2D. BodyDef, FixtureDef, collision filtering, and Projectile creation finished.

---
+++ core/src/com/pottda/game/actorFactory/Box2DActorFactory.java
@@ -0,0 +1,213 @@
+package com.pottda.game.actorFactory;
+
+import com.badlogic.gdx.physics.box2d.*;
+import com.pottda.game.controller.AbstractController;
+import com.pottda.game.controller.ProjectileController;
+import com.pottda.game.model.ActorFactory;
+import com.pottda.game.model.Inventory;
+import com.pottda.game.model.Projectile;
+import com.pottda.game.physicsBox2D.Box2DPhysicsProjectile;
+import com.pottda.game.view.ViewActor;
+
+public class Box2DActorFactory extends ActorFactory{
+    // Constants
+    private final static float GRAVITY = 0f;
+
+    public final static int PLAYER_TEAM = 0;
+    public final static int ENEMY_TEAM = 1;
+
+    private final static float CHARACTER_LINEAR_DAMPING = 0f;
+    private final static float CHARACTER_ANGULAR_DAMPING = 0.1f;
+    private final static float CHARACTER_RADIUS = 0.5f;
+    private final static float CHARACTER_DENSITY = 1f;
+    private final static float CHARACTER_FRICTION = 0.25f;
+    private final static float CHARACTER_BOUNCINESS = 0.1f;
+    private static Filter CHARACTER_PLAYER_FILTER;
+    private static Filter CHARACTER_ENEMY_FILTER;
+
+    private final static float PROJECTILE_DAMPING = 0f;
+    private final static float PROJECTILE_RADIUS = 0.04f;
+    private final static float PROJECTILE_DENSITY = 1f;
+    private final static float PROJECTILE_FRICTION = 1f;
+    private final static float PROJECTILE_BOUNCINESS_DEFAULT = 0f;
+    private final static float PROJECTILE_BOUNCINESS_BOUNCY = 1f;
+    private static Filter PROJECTILE_PLAYER_FILTER;
+    private static Filter PROJECTILE_ENEMY_FILTER;
+    private static Filter PROJECTILE_PENETRATION_FILTER;
+
+    private final static float PROJECTILE_SENSOR_RADIUS = 0.05f;
+    private final static float PROJECTILE_SENSOR_DENSITY = 0f;
+
+    private final static float OBSTACLE_HALF_WIDTH = 0.5f;
+    private final static float OBSTACLE_HALF_HEIGHT = 0.5f;
+    private final static float OBSTACLE_FRICTION = 0.25f;
+    private final static float OBSTACLE_BOUNCINESS = 0f;
+
+
+    private final World world;
+
+    private BodyDef characterBodyDef;
+    private BodyDef projectileBodyDef;
+    private BodyDef obstacleBodyDef;
+
+    private FixtureDef characterFixtureDef;
+    private FixtureDef projectileFixtureDef;
+    private FixtureDef projectileSensorFixtureDef;
+    private FixtureDef obstacleFixtureDef;
+
+    public Box2DActorFactory(World world){
+        this.world = world;
+        filterCategoryInit();
+        bodyDefInit();
+        fixtureDefInit();
+    }
+
+    @Override
+    public AbstractController buildEnemy() {
+        return null;
+    }
+
+    @Override
+    public AbstractController buildPlayer() {
+        return null;
+    }
+
+    @Override
+    public ProjectileController buildProjectile(int team, boolean bounces, boolean penetrates) {
+        // Create body
+        Body body = world.createBody(projectileBodyDef);
+
+        // Determine bounciness
+        if(bounces){
+            projectileFixtureDef.restitution = PROJECTILE_BOUNCINESS_BOUNCY;
+        } else {
+            projectileFixtureDef.restitution = PROJECTILE_BOUNCINESS_DEFAULT;
+        }
+
+        // Determine collision filtering
+        if(penetrates){
+            projectileFixtureDef.filter.categoryBits = PROJECTILE_PENETRATION_FILTER.categoryBits;
+            projectileFixtureDef.filter.maskBits = PROJECTILE_PENETRATION_FILTER.maskBits;
+        } else if (team == ENEMY_TEAM){
+            projectileFixtureDef.filter.categoryBits = PROJECTILE_ENEMY_FILTER.categoryBits;
+            projectileFixtureDef.filter.maskBits = PROJECTILE_ENEMY_FILTER.maskBits;
+        } else {
+            projectileFixtureDef.filter.categoryBits = PROJECTILE_PLAYER_FILTER.categoryBits;
+            projectileFixtureDef.filter.maskBits = PROJECTILE_PLAYER_FILTER.maskBits;
+        }
+
+        // Give body fixtures and calculate mass
+        body.createFixture(projectileFixtureDef);
+        body.createFixture(projectileSensorFixtureDef);
+        body.resetMassData();
+
+        Box2DPhysicsProjectile physics = new Box2DPhysicsProjectile(body);
+
+        Projectile model = new Projectile(physics, 0, null);
+
+        ViewActor view = new ViewActor();
+
+        ProjectileController controller = new ProjectileController(model, view);
+
+        return controller;
+    }
+
+    @Override
+    public AbstractController buildObstacle() {
+        return null;
+    }
+
+    private void filterCategoryInit(){
+        CHARACTER_PLAYER_FILTER = new Filter();
+        CHARACTER_ENEMY_FILTER = new Filter();
+        PROJECTILE_PLAYER_FILTER = new Filter();
+        PROJECTILE_ENEMY_FILTER = new Filter();
+        PROJECTILE_PENETRATION_FILTER = new Filter();
+
+        CHARACTER_PLAYER_FILTER.categoryBits = 0x0001;
+        CHARACTER_ENEMY_FILTER.categoryBits = 0x0002;
+        PROJECTILE_PLAYER_FILTER.categoryBits = 0x0004;
+        PROJECTILE_ENEMY_FILTER.categoryBits = 0x0008;
+        PROJECTILE_PENETRATION_FILTER.categoryBits = 0x0010;
+
+        // Player collides with Enemy Character and Projectile
+        CHARACTER_PLAYER_FILTER.maskBits = (short)((int)CHARACTER_ENEMY_FILTER.categoryBits +
+                (int)PROJECTILE_ENEMY_FILTER.categoryBits);
+
+        // Enemy collides with itself, Player Character and Projectile
+        CHARACTER_ENEMY_FILTER.maskBits = (short)((int)CHARACTER_PLAYER_FILTER.categoryBits +
+                (int)CHARACTER_ENEMY_FILTER.categoryBits + (int)PROJECTILE_PLAYER_FILTER.categoryBits);
+
+        // Player Projectile collides with Enemy Character
+        PROJECTILE_PLAYER_FILTER.maskBits = CHARACTER_ENEMY_FILTER.categoryBits;
+
+        // Enemy Projectile collides with Player Character
+
+        PROJECTILE_ENEMY_FILTER.maskBits = CHARACTER_PLAYER_FILTER.categoryBits;
+
+        // Projectiles with the 'penetrates' flag only collide with the terrain
+        PROJECTILE_PENETRATION_FILTER.maskBits = 0x0000;
+    }
+
+    private void bodyDefInit(){
+        characterBodyDef = new BodyDef();
+        projectileBodyDef = new BodyDef();
+        obstacleBodyDef = new BodyDef();
+
+        // Character (enemy and player)
+        characterBodyDef.type = BodyDef.BodyType.DynamicBody;
+        characterBodyDef.linearDamping = CHARACTER_LINEAR_DAMPING;
+        characterBodyDef.angularDamping = CHARACTER_ANGULAR_DAMPING;
+        characterBodyDef.gravityScale = GRAVITY;
+        characterBodyDef.fixedRotation = true;
+
+        // Projectile
+        projectileBodyDef.type = BodyDef.BodyType.DynamicBody;
+        projectileBodyDef.linearDamping = PROJECTILE_DAMPING;
+        projectileBodyDef.angularDamping = PROJECTILE_DAMPING;
+        projectileBodyDef.gravityScale = GRAVITY;
+        projectileBodyDef.fixedRotation = true;
+
+        // Obstacle
+        obstacleBodyDef.type = BodyDef.BodyType.StaticBody;
+    }
+
+    private void fixtureDefInit() {
+        characterFixtureDef = new FixtureDef();
+        projectileFixtureDef = new FixtureDef();
+        obstacleFixtureDef = new FixtureDef();
+
+        PolygonShape tempPolygon;
+        CircleShape tempCircle;
+
+        // Character (enemy and player)
+        tempCircle = new CircleShape();
+        tempCircle.setRadius(CHARACTER_RADIUS);
+        characterFixtureDef.shape = tempCircle;
+        characterFixtureDef.density = CHARACTER_DENSITY;
+        characterFixtureDef.friction = CHARACTER_FRICTION;
+        characterFixtureDef.restitution = CHARACTER_BOUNCINESS;
+
+        // Projectile, collision part
+        tempCircle = new CircleShape();
+        tempCircle.setRadius(PROJECTILE_RADIUS);
+        projectileFixtureDef.shape = tempCircle;
+        projectileFixtureDef.density = PROJECTILE_DENSITY;
+        projectileFixtureDef.friction = PROJECTILE_FRICTION;
+        projectileFixtureDef.restitution = PROJECTILE_BOUNCINESS_DEFAULT;
+
+        // Projectile, sensor part
+        tempCircle = new CircleShape();
+        tempCircle.setRadius(PROJECTILE_SENSOR_RADIUS);
+        projectileSensorFixtureDef.shape = tempCircle;
+        projectileSensorFixtureDef.density = PROJECTILE_SENSOR_DENSITY;
+        projectileSensorFixtureDef.isSensor = true;
+
+        // Obstacle
+        tempPolygon = new PolygonShape();
+        tempPolygon.setAsBox(OBSTACLE_HALF_WIDTH, OBSTACLE_HALF_HEIGHT);
+        obstacleFixtureDef.shape = tempPolygon;
+        obstacleFixtureDef.friction = OBSTACLE_FRICTION;
+        obstacleFixtureDef.restitution = OBSTACLE_BOUNCINESS;
+    }
+}
