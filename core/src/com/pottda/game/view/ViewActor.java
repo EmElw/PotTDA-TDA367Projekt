3f9bd0c862849ec68eb9865977154ed37c259c31
Phones <farmerfaz@gmail.com>
2017-05-08 15:13:22 +0200

    Added some comments, for real this time

--- core/src/com/pottda/game/view/ViewActor.java
+++ core/src/com/pottda/game/view/ViewActor.java
@@ -2,11 +2,30 @@ package com.pottda.game.view;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+import javax.vecmath.Vector2f;
 
 /**
  *
  */
 public class ViewActor extends Actor {
 
+    /**
+     * Sets the position of the actor
+     *
+     * @param xPosition The x position of the actor
+     * @param yPosition The y position of the actor
+     */
+    public void setPoint(float xPosition, float yPosition) {
+        this.setPosition(xPosition, yPosition);
+    }
+
+    /**
+     * Sets the angle which the actor is looking
+     *
+     * @param degrees   The angle that the actor is looking
+     */
+    public void setAngle(float degrees) {
+        this.setRotation(degrees);
+    }
 
 }
