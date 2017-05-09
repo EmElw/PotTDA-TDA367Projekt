e8e883426178366a2bdb19127797d8ff2806f9b2
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-09 18:03:01 +0200

    Implemented IndexedA* pathfinding, still no output.

--- core/src/com/pottda/game/pathfindingGDXAI/GDXAIPathfinder.java
+++ core/src/com/pottda/game/pathfindingGDXAI/GDXAIPathfinder.java
@@ -1,30 +1,55 @@
 package com.pottda.game.pathfindingGDXAI;
 
+import com.badlogic.gdx.ai.GdxAI;
+import com.badlogic.gdx.ai.pfa.*;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder;
+import com.badlogic.gdx.utils.SortedIntList;
 import com.pottda.game.model.ModelActor;
 import com.pottda.game.model.Pathfinder;
+import com.pottda.game.view.LibGDXApp;
 
 import javax.vecmath.Vector2f;
+import java.util.Iterator;
 
 public class GDXAIPathfinder implements Pathfinder {
     private final GDXAIGraph graph;
+    private ModelActor goal;
+    private IndexedAStarPathFinder indexedAStarPathFinder;
 
     public GDXAIPathfinder(ModelActor goal, int worldWidth, int worldHeight) {
         this.goal = goal;
         graph = new GDXAIGraph(worldWidth, worldHeight);
+        try {
+            indexedAStarPathFinder = new IndexedAStarPathFinder(graph);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    public ModelActor goal;
 
     @Override
     public Vector2f getPath(Vector2f location) {
+        Heuristic<SortedIntList.Node> heuristic;
+        GraphPath<SortedIntList.Node> outPath;
+
+        heuristic = new MyEuclideanDistanceApproximation(graph);
+        outPath = new DefaultGraphPath<SortedIntList.Node>();
+
+        try {
+            indexedAStarPathFinder.searchNodePath(graph.getNode(location),
+                    graph.getNode(goal.getPosition()), heuristic, outPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
 
         return null;
     }
 
     @Override
     public void setObstacle(Vector2f obstacle) {
-        int x = (int)obstacle.x;
-        int y = (int)obstacle.y;
+        int x = (int) obstacle.x;
+        int y = (int) obstacle.y;
 
         // Assumes objects are not much larger than a grid 'tile'
 
@@ -33,13 +58,13 @@ public class GDXAIPathfinder implements Pathfinder {
 
         graph.setObstacle(x, y);
 
-        if (!alignedX){
+        if (!alignedX) {
             graph.setObstacle(x + 1, y);
-            if(!alignedY){
+            if (!alignedY) {
                 graph.setObstacle(x + 1, y + 1);
             }
         }
-        if (!alignedY){
+        if (!alignedY) {
             graph.setObstacle(x, y + 1);
         }
     }
