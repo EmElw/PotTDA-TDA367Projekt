7b5df659a57104a7d719c747fbb01fad41dd7790
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-09 18:02:14 +0200

    Prepared for IndexedA* (pathfinding) by implementing IndexedGraph.

--- core/src/com/pottda/game/pathfindingGDXAI/GDXAIGraph.java
+++ core/src/com/pottda/game/pathfindingGDXAI/GDXAIGraph.java
@@ -2,28 +2,34 @@ package com.pottda.game.pathfindingGDXAI;
 
 import com.badlogic.gdx.ai.pfa.Connection;
 import com.badlogic.gdx.ai.pfa.Graph;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedGraph;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.SortedIntList;
 
-public class GDXAIGraph implements Graph {
+import javax.vecmath.Vector2f;
+
+public class GDXAIGraph implements IndexedGraph<SortedIntList.Node>, Graph<SortedIntList.Node> {
     public final static float OBSTACLE_COST = 1000f;
+    public final static float EMPTY_COST = 1f;
     public final static int MAX_CONNECTIONS_PER_NODE = 4;
 
-    private Array<GDXAIConnection> connections;
-    private Array<SortedIntList.Node> nodes;
-    private final int width;
-    private final int height;
+    protected Array<GDXAIConnection> connections;
+    protected Array<SortedIntList.Node> nodes;
+    protected final int width;
+    protected final int height;
 
     public GDXAIGraph(int width, int height) {
         this.width = width;
         this.height = height;
 
         nodes = new Array<SortedIntList.Node>(width * height);
+        nodes.ordered = true;
         for (int i = 0, n = width * height; i < n; i++) {
             nodes.add(new SortedIntList.Node());
         }
 
         connections = new Array<GDXAIConnection>();
+        connections.ordered = true;
         // Set up connections between neighbouring nodes
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -33,6 +39,7 @@ public class GDXAIGraph implements Graph {
                         connections.add(new GDXAIConnection());
                         connections.get(connections.size - 1).fromNode = nodes.get(x * width + y);
                         connections.get(connections.size - 1).toNode = nodes.get(i * width + y);
+                        connections.get(connections.size - 1).cost = EMPTY_COST;
                     }
                 }
                 // Connections up and down
@@ -41,23 +48,27 @@ public class GDXAIGraph implements Graph {
                         connections.add(new GDXAIConnection());
                         connections.get(connections.size - 1).fromNode = nodes.get(x * width + y);
                         connections.get(connections.size - 1).toNode = nodes.get(x * width + i);
+                        connections.get(connections.size - 1).cost = EMPTY_COST;
                     }
                 }
             }
         }
     }
 
-    @Override
-    public Array<GDXAIConnection> getConnections(Object fromNode) {
-        Array<GDXAIConnection> returnArray = new Array<GDXAIConnection>();
+//    public Array<GDXAIConnection> getConnections(Object fromNode) {
+//        Array<GDXAIConnection> returnArray = new Array<GDXAIConnection>();
+//
+//        for (int i = 0, n = connections.size; i < n && returnArray.size < MAX_CONNECTIONS_PER_NODE; i++) {
+//            if (connections.get(i).fromNode.equals(fromNode)) {
+//                returnArray.add(connections.get(i));
+//            }
+//        }
+//
+//        return returnArray;
+//    }
 
-        for (int i = 0, n = connections.size; i < n && returnArray.size < MAX_CONNECTIONS_PER_NODE; i++) {
-            if (connections.get(i).fromNode.equals(fromNode)) {
-                returnArray.add(connections.get(i));
-            }
-        }
-
-        return returnArray;
+    public SortedIntList.Node getNode(Vector2f position) {
+        return nodes.get(Math.round(position.x) * width + Math.round(position.y));
     }
 
     public void setObstacle(int x, int y) {
@@ -69,5 +80,29 @@ public class GDXAIGraph implements Graph {
                 changedConntections++;
             }
         }
+    }
+
+    @Override
+    public int getIndex(SortedIntList.Node node) {
+        return nodes.indexOf(node, false);
+    }
+
+    @Override
+    public int getNodeCount() {
+        return nodes.size;
+    }
+
+    @Override
+    public Array<Connection<SortedIntList.Node>> getConnections(SortedIntList.Node fromNode) {
+        Array<Connection<SortedIntList.Node>> returnArray =
+                new Array<Connection<SortedIntList.Node>>();
+
+        for (int i = 0, n = connections.size; i < n && returnArray.size < MAX_CONNECTIONS_PER_NODE; i++) {
+            if (connections.get(i).fromNode.equals(fromNode)) {
+                returnArray.add(connections.get(i));
+            }
+        }
+
+        return returnArray;
     }
 }
