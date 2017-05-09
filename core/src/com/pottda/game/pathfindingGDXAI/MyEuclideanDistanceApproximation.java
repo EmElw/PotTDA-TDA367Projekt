41ecb1ba046a1f04279498a60ac74a1f47c23bc6
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-09 18:01:11 +0200

    Added a Euclidean Distance-heuristics class (used in pathfinding, in this case A*).

---
+++ core/src/com/pottda/game/pathfindingGDXAI/MyEuclideanDistanceApproximation.java
@@ -0,0 +1,31 @@
+package com.pottda.game.pathfindingGDXAI;
+
+import com.badlogic.gdx.ai.pfa.Heuristic;
+import com.badlogic.gdx.utils.SortedIntList;
+
+public class MyEuclideanDistanceApproximation implements Heuristic<SortedIntList.Node> {
+    private final static float NORMAL_COST = 1f;
+    private int width;
+    private int height;
+    private GDXAIGraph graph;
+
+    public MyEuclideanDistanceApproximation(GDXAIGraph graph) {
+        this.graph = graph;
+        width = graph.width;
+        height = graph.height;
+    }
+
+    @Override
+    public float estimate(SortedIntList.Node node, SortedIntList.Node endNode) {
+        int startX = graph.nodes.indexOf(node, false) / width;
+        int startY = graph.nodes.indexOf(node, false) % width;
+
+        int goalX = graph.nodes.indexOf(endNode, false) / width;
+        int goalY = graph.nodes.indexOf(endNode, false) % width;
+
+        int dx = Math.abs(startX - goalX);
+        int dy = Math.abs(startY - goalY);
+
+        return NORMAL_COST * (float) Math.sqrt(dx * dx + dy * dy);
+    }
+}
