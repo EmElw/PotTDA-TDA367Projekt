cbdc80fa6348e01ec552be70cfc3c8c399985c00
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-12 22:05:34 +0200

    Added cooldown to calls to Pathfinder (once per second per enemy).

--- core/src/com/pottda/game/controller/PathfindingAIController.java
+++ core/src/com/pottda/game/controller/PathfindingAIController.java
@@ -6,15 +6,27 @@ import com.pottda.game.pathfindingGDXAI.GDXAIPathfinder;
 import com.pottda.game.view.ViewActor;
 
 public class PathfindingAIController extends AIController {
+    private final static int UPDATE_COOLDOWN_TIME = 1000;
+
     private GDXAIPathfinder pathfinder;
+    private long lastUpdateTime;
+
     public PathfindingAIController(ModelActor modelActor, ViewActor viewActor, GDXAIPathfinder pathfinder) {
         super(modelActor, viewActor);
         this.pathfinder = pathfinder;
     }
 
     public void onNewFrame(){
+        if (System.currentTimeMillis() - lastUpdateTime > UPDATE_COOLDOWN_TIME){
+            lastUpdateTime = System.currentTimeMillis();
+            updateVectors();
+        }
+
+        super.onNewFrame();
+    }
+
+    private void updateVectors(){
         movementVector = pathfinder.getPath(modelActor.getPosition());
         attackVector = movementVector;
-        super.onNewFrame();
     }
 }
