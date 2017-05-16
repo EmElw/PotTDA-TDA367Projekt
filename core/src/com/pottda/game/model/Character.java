f78a17fd5bc5cc6910a2e16cc0a10534dba6b625
Gustav Lahti <gustav.lahti@gmail.com>
2017-05-16 15:18:53 +0200

    Added death for Characters.

--- core/src/com/pottda/game/model/Character.java
+++ core/src/com/pottda/game/model/Character.java
@@ -77,7 +77,7 @@ public class Character extends ModelActor {
     public void takeDamage(int incomingDamage) {
         currentHealth -= incomingDamage;
         if (currentHealth <= 0) {
-            // TODO Die
+            shouldBeRemoved = true;
         }
     }
 
