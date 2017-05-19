b63655369071b18ec51d25a9c02237c0d9d14a27
Phones <farmerfaz@gmail.com>
2017-05-19 17:02:24 +0200

    Converted from Java's BufferedImage to LibGDX's Pixmap

--- core/src/com/pottda/game/view/AtlasCreator.java
+++ core/src/com/pottda/game/view/AtlasCreator.java
@@ -1,30 +1,42 @@
 package com.pottda.game.view;
 
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.pottda.game.model.Item;
 
 import javax.vecmath.Point2i;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.util.List;
 
 /**
  * Created by Mr Cornholio on 19/05/2017.
  */
 public class AtlasCreator {
-    private BufferedImage img;
+    private Pixmap itemPix;
     private final static int size = 25;
 
-    public void createImageFromItem(Item item) {
+    
+
+    private Texture createTextureForItem(Item item) {
         List<Point2i> basePos = item.getBasePositions();
         List<Point2i> baseOut = item.getBaseOutputs();
 
-        img = getBoundingBox(basePos, baseOut);
-        Graphics g = img.getGraphics();
+        getBoundingBox(basePos, baseOut);
 
+        itemPix.setColor(255, 0, 0, 1);
+        for (Point2i i : basePos) {
+            itemPix.fillRectangle(i.getX()*25, i.getY()*25, size, size);
+        }
 
+        itemPix.setColor(0, 0, 255, 1);
+        for (Point2i i : baseOut) {
+            itemPix.fillRectangle(i.getX()*25, i.getY()*25, size, size);
+        }
+
+        Texture itemTex = new Texture(itemPix);
+        return itemTex;
     }
 
-    private BufferedImage getBoundingBox(List<Point2i> basePos, List<Point2i> baseOut) {
+    private void getBoundingBox(List<Point2i> basePos, List<Point2i> baseOut) {
         int xLow = 0, xHigh = 0, yLow = 0, yHigh = 0, boundingX = 0, boundingY = 0;
         for (Point2i i: basePos) {
             xLow = Math.min(xLow, i.getX());
@@ -46,10 +58,10 @@ public class AtlasCreator {
 
         boundingX = (xHigh - xLow)*25;
         boundingY = (yHigh - yLow)*25;
-        return new BufferedImage(boundingX, boundingY, BufferedImage.TYPE_INT_ARGB);
+        itemPix = new Pixmap(boundingX, boundingY, Pixmap.Format.RGBA8888);
     }
 
-    public BufferedImage getImg() {
-        return  img;
+    public Pixmap getImg() {
+        return  itemPix;
     }
 }
