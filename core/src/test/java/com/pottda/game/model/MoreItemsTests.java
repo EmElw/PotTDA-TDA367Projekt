e7a259cad46ffd59f610f02240f54e7034a184cd
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-05-25 15:42:21 +0200

    Added tests for EnemySimpleCannon and HealthItem

--- core/src/test/java/com/pottda/game/model/MoreItemsTests.java
+++ core/src/test/java/com/pottda/game/model/MoreItemsTests.java
@@ -8,8 +8,10 @@ import com.pottda.game.model.items.HealthItem;
 import com.pottda.game.model.items.PenetratingCannon;
 import com.pottda.game.model.items.ProjectileSpeedItem;
 import com.pottda.game.model.items.SpeedItem;
+import com.pottda.game.model.items.SupportItem;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,8 +21,8 @@ public class MoreItemsTests {
 
     private AttackItem bouncingBallCannon;
     private GenericProjectileModifier damageItem;
-    private Item enemySimpleCannon;
-    private Item healthItem;
+    private AttackItem enemySimpleCannon;
+    private SupportItem healthItem;
     private Item penetratingCannon;
     private Item projectileSpeedItem;
     private Item speedItem;
@@ -39,7 +41,7 @@ public class MoreItemsTests {
     @Test
     public void testBouncingBallCannon() {
         assertFalse(bouncingBallCannon.isPrimaryAttack);
-        assertTrue(!bouncingBallCannon.bounces);
+        assertFalse(bouncingBallCannon.bounces);
         assertEquals(bouncingBallCannon.basePositions, null);
         assertEquals(bouncingBallCannon.baseOutputs, null);
         assertFalse(bouncingBallCannon.cooldownMS == 300);
@@ -48,7 +50,7 @@ public class MoreItemsTests {
         bouncingBallCannon.init();
 
         assertTrue(bouncingBallCannon.isPrimaryAttack);
-        assertFalse(!bouncingBallCannon.bounces);
+        assertTrue(bouncingBallCannon.bounces);
         assertEquals(bouncingBallCannon.basePositions.size(), 2);
         assertEquals(bouncingBallCannon.baseOutputs.size(), 1);
         assertTrue(bouncingBallCannon.cooldownMS == 300);
@@ -76,6 +78,35 @@ public class MoreItemsTests {
         Projectile p = new Projectile(10, new ArrayList<ProjectileListener>());
         damageItem.onAttack(p);
         assertTrue(p.damage >= 10);
+    }
+
+    @Test
+    public void testEnemySimpleCannon() {
+        assertFalse(enemySimpleCannon.isPrimaryAttack);
+        assertFalse(enemySimpleCannon.bounces);
+        assertEquals(enemySimpleCannon.basePositions, null);
+        assertEquals(enemySimpleCannon.baseOutputs, null);
+        assertFalse(enemySimpleCannon.cooldownMS == 300);
+        assertFalse(enemySimpleCannon.damage == 10);
+
+        enemySimpleCannon.init();
+
+        assertTrue(enemySimpleCannon.isPrimaryAttack);
+        assertFalse(enemySimpleCannon.bounces);
+        assertEquals(enemySimpleCannon.basePositions.size(), 1);
+        assertEquals(enemySimpleCannon.baseOutputs.size(), 1);
+        assertTrue(enemySimpleCannon.cooldownMS == 500);
+        assertTrue(enemySimpleCannon.damage == 20);
+    }
+
+    @Test
+    public void testHealthItem() {
+        healthItem.init();
+        assertTrue(healthItem.statMap.containsKey(Stat.HEALTH));
+        assertTrue(healthItem.dropRate == 0.25f * 0.75f
+                || healthItem.dropRate == 0.5f * 0.75f
+                || healthItem.dropRate == 0.1f * 0.75f);
+        assertTrue(healthItem.name.contains("Health Module"));
     }
 
 }
