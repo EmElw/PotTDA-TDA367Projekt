e152884519efa82e0fd746d83d337fb18241b13a
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-05-25 15:47:28 +0200

    Moved tests to ItemTest.java, added PenetratingCannon test

--- core/src/test/java/com/pottda/game/model/ItemTest.java
+++ core/src/test/java/com/pottda/game/model/ItemTest.java
@@ -1,7 +1,17 @@
 package com.pottda.game.model;
 
+import com.pottda.game.model.items.BouncingBallCannon;
+import com.pottda.game.model.items.DamageItem;
 import com.pottda.game.model.items.DemoItemA;
 import com.pottda.game.model.items.DemoItemB;
+import com.pottda.game.model.items.EnemySimpleCannon;
+import com.pottda.game.model.items.GenericProjectileModifier;
+import com.pottda.game.model.items.HealthItem;
+import com.pottda.game.model.items.PenetratingCannon;
+import com.pottda.game.model.items.ProjectileSpeedItem;
+import com.pottda.game.model.items.SpeedItem;
+import com.pottda.game.model.items.SupportItem;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +21,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 /**
  * Test the spatial properties of the Item class
  */
@@ -18,6 +30,14 @@ public class ItemTest {
 
     private Item a;
     private Item b;
+
+    private AttackItem bouncingBallCannon;
+    private GenericProjectileModifier damageItem;
+    private AttackItem enemySimpleCannon;
+    private SupportItem healthItem;
+    private AttackItem penetratingCannon;
+    private Item projectileSpeedItem;
+    private Item speedItem;
 
     @Before
     public void setUp() {
@@ -34,6 +54,14 @@ public class ItemTest {
         b.setOrientation(3);
         b.setX(8);
         b.setY(4);
+
+        bouncingBallCannon = new BouncingBallCannon();
+        damageItem = new DamageItem();
+        enemySimpleCannon = new EnemySimpleCannon();
+        healthItem = new HealthItem();
+        penetratingCannon = new PenetratingCannon();
+        projectileSpeedItem = new ProjectileSpeedItem();
+        speedItem = new SpeedItem();
     }
 
     @Test
@@ -61,4 +89,97 @@ public class ItemTest {
         Assert.assertEquals(outputA.get(0), Integer.valueOf(40));
         Assert.assertEquals(outputB.get(0), Integer.valueOf(19));
     }
+
+    @Test
+    public void testBouncingBallCannon() {
+        assertFalse(bouncingBallCannon.isPrimaryAttack);
+        assertFalse(bouncingBallCannon.bounces);
+        assertEquals(bouncingBallCannon.basePositions, null);
+        assertEquals(bouncingBallCannon.baseOutputs, null);
+        assertFalse(bouncingBallCannon.cooldownMS == 300);
+        assertFalse(bouncingBallCannon.damage == 10);
+
+        bouncingBallCannon.init();
+
+        assertTrue(bouncingBallCannon.isPrimaryAttack);
+        assertTrue(bouncingBallCannon.bounces);
+        assertEquals(bouncingBallCannon.basePositions.size(), 2);
+        assertEquals(bouncingBallCannon.baseOutputs.size(), 1);
+        assertTrue(bouncingBallCannon.cooldownMS == 300);
+        assertTrue(bouncingBallCannon.damage == 10);
+
+        bouncingBallCannon.setX(2);
+        bouncingBallCannon.setY(3);
+        bouncingBallCannon.setOrientation(1);
+        assertTrue(bouncingBallCannon.getX() == 2);
+        assertTrue(bouncingBallCannon.getY() == 3);
+        assertTrue(bouncingBallCannon.getOrientation() == 1);
+    }
+
+    @Test
+    public void testDamageItem() {
+        // Drop rate: 0.25f, 0.5f, 0.1f
+        damageItem.init();
+        assertTrue(damageItem.dropRate == 0.25f * 0.5f
+                || damageItem.dropRate == 0.5f * 0.5f
+                || damageItem.dropRate == 0.1f * 0.5f);
+        assertTrue(damageItem.name.contains("Damage Module"));
+        assertTrue(damageItem.basePositions.size() == 2 || damageItem.basePositions.size() == 1);
+        assertTrue(damageItem.isProjectileModifier);
+
+        Projectile p = new Projectile(10, new ArrayList<ProjectileListener>());
+        damageItem.onAttack(p);
+        assertTrue(p.damage >= 10);
+    }
+
+    @Test
+    public void testEnemySimpleCannon() {
+        assertFalse(enemySimpleCannon.isPrimaryAttack);
+        assertFalse(enemySimpleCannon.bounces);
+        assertEquals(enemySimpleCannon.basePositions, null);
+        assertEquals(enemySimpleCannon.baseOutputs, null);
+        assertFalse(enemySimpleCannon.cooldownMS == 500);
+        assertFalse(enemySimpleCannon.damage == 20);
+
+        enemySimpleCannon.init();
+
+        assertTrue(enemySimpleCannon.isPrimaryAttack);
+        assertFalse(enemySimpleCannon.bounces);
+        assertEquals(enemySimpleCannon.basePositions.size(), 1);
+        assertEquals(enemySimpleCannon.baseOutputs.size(), 1);
+        assertTrue(enemySimpleCannon.cooldownMS == 500);
+        assertTrue(enemySimpleCannon.damage == 20);
+    }
+
+    @Test
+    public void testHealthItem() {
+        healthItem.init();
+        assertTrue(healthItem.statMap.containsKey(Stat.HEALTH));
+        assertTrue(healthItem.dropRate == 0.25f * 0.75f
+                || healthItem.dropRate == 0.5f * 0.75f
+                || healthItem.dropRate == 0.1f * 0.75f);
+        assertTrue(healthItem.name.contains("Health Module"));
+    }
+
+    @Test
+    public void testPenetratingCannon() {
+        assertFalse(penetratingCannon.isPrimaryAttack);
+        assertFalse(penetratingCannon.piercing);
+        assertFalse(penetratingCannon.bounces);
+        assertEquals(penetratingCannon.basePositions, null);
+        assertEquals(penetratingCannon.baseOutputs, null);
+        assertFalse(penetratingCannon.cooldownMS == 300);
+        assertFalse(penetratingCannon.damage == 10);
+
+        penetratingCannon.init();
+
+        assertTrue(penetratingCannon.isPrimaryAttack);
+        assertTrue(penetratingCannon.piercing);
+        assertFalse(penetratingCannon.bounces);
+        assertEquals(penetratingCannon.basePositions.size(), 2);
+        assertEquals(penetratingCannon.baseOutputs.size(), 1);
+        assertTrue(penetratingCannon.cooldownMS == 300);
+        assertTrue(penetratingCannon.damage == 10);
+    }
+
 }
